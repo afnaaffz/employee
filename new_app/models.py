ddf34f05cc9cae68ec85f1b4a2dc9265472e1f8f
afnaaffz <mailatafna@gmail.com>
2024-06-30 19:48:33 +0530

    neww

--- new_app/models.py
+++ new_app/models.py
@@ -7,7 +7,7 @@ class Employee(models.Model):
     current_project = models.CharField(max_length=100)
     projects_done = models.IntegerField()
     address = models.TextField()
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=10)
     salary = models.FloatField()
     promotion_details = models.TextField()
 
